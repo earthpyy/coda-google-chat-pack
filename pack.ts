324704a2a85061c0821b0816dd3d533cd9fc597f
Preeti Yuankrathok <preetisatit@gmail.com>
2022-03-16 00:23:07 +0700

    Add ability to send to webhook

--- pack.ts
+++ pack.ts
@@ -1,3 +1,49 @@
 import * as coda from '@codahq/packs-sdk'
 
 export const pack = coda.newPack()
+
+pack.addNetworkDomain('googleapis.com')
+
+pack.addFormula({
+  name: 'SendToWebHook',
+  description: "Send message to Google Chat's webhook",
+  isAction: true,
+  resultType: coda.ValueType.String,
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: 'webhookUrl',
+      description: 'URL for webhook (should begins with `https://chat.googleapis.com`)',
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: 'message',
+      description: 'Message to send (as plain text or JSON format)',
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: 'threadKey',
+      description: 'Thread key in order to send multiple messages to the same thread',
+      optional: true,
+    }),
+  ],
+  execute: async ([webhookUrl, message, threadKey], context) => {
+    const isJSON = message.startsWith('{') && message.endsWith('}')
+    const payload = isJSON ? JSON.parse(message) : { text: message }
+
+    if (threadKey) {
+      webhookUrl = coda.withQueryParams(webhookUrl, {
+        threadKey,
+      })
+    }
+
+    const response = await context.fetcher.fetch({
+      method: 'POST',
+      url: webhookUrl,
+      cacheTtlSecs: 0,
+      body: JSON.stringify(payload),
+    })
+
+    return response.body
+  },
+})
